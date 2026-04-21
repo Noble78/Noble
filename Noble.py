86c2cb377e41578870b200cf2ceba1d427a0a6ea
unknown <igwenoble50@gmail.comgit config --global user.name Noble78  git config --global user.email igwenoble50@gmail.comgit config --user.email igwenoble50@gmail.com>
2026-04-21 13:34:48 +0100

    Updated README

--- Noble.py
+++ Noble.py
@@ -1,7 +1,12 @@
 
-_fullname = input('Enter Your Name:')
-_phoneno =  input('Enter your Phone No:')
-if _fullname == 'NOBLE IGWE' and _phoneno =='09161075681' :
-    print('Welcome NOBLE')
-else:      
-    print('')
+_revenue = 5000
+_expenses = 1200
+_tax_rate = 0.25
+_profit = _revenue - _expenses
+print('Profit =', _profit)
+_tax_obligation = _profit * _tax_rate
+print( 'Tax obligation =', _tax_obligation)
+_net_income = _profit - _tax_obligation
+print('Net imcome =', _net_income)
+_daily_spending_allowance = _net_income/ 30
+print('Daily Allowance =', _daily_spending_allowance)
