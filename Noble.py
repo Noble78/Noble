a1fc49c1baa1e635534328b58573781bb1d5160d
unknown <igwenoble50@gmail.comgit config --global user.name Noble78  git config --global user.email igwenoble50@gmail.comgit config --user.email igwenoble50@gmail.com>
2026-04-21 12:45:28 +0100

    python code

---
+++ Noble.py
@@ -0,0 +1,7 @@
+
+_fullname = input('Enter Your Name:')
+_phoneno =  input('Enter your Phone No:')
+if _fullname == 'NOBLE IGWE' and _phoneno =='09161075681' :
+    print('Welcome NOBLE')
+else:      
+    print('')
