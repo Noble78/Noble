f0f726729f73f0561b5741a4c366ec5f50087685
Noble78 <igwenoble50@gmail.com>
2026-08-12 10:20:11 +0100

    Resolved misplacement

--- payment.py
+++
@@ -1,9 +0,0 @@
-class Login:
-    def authenticate(self, username, password):
-        if username == "admin" and password == "1234":
-            return "Login successful"
-
-        return "Invalid username or password"
-
-login = Login()
-print(login.authenticate("admin", "1234"))
